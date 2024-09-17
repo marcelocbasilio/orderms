84d62f48f40318495c278a16ca832233c0dedc4c
Marcelo Cordeiro Basílio <marcelocbasilio@gmail.com>
2024-09-17 18:59:34 -0300

    Initial Configuration of the application and MongoDb and RabbitMQ.
    Signed-off-by: Marcelo Cordeiro Basílio <marcelocbasilio@gmail.com>

---
+++ src/main/java/com/marcelocbasilio/orderms/config/RabbitMqConfig.java
@@ -0,0 +1,9 @@
+package com.marcelocbasilio.orderms.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    public static final String ORDER_CREATED_QUEUE = "btg-pactual-order-created";
+}
